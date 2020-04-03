f79c1d7232ad88602bec7e422f063a2a575887d1
pedro-inacio11 <pedro.inacio11@gmail.com>
2020-04-02 22:01:00 -0300

    Add Servicos

---
+++ src/main/java/com/phinacio/pontointeligente/api/service/EmpresaService.java
@@ -0,0 +1,25 @@
+package com.phinacio.pontointeligente.api.service;
+
+import com.phinacio.pontointeligente.api.entities.Empresa;
+
+import java.util.Optional;
+
+public interface EmpresaService {
+
+    /**
+     * Retorna uma empresa dado um CNPJ.
+     *
+     * @param cnpj
+     * @return Optional<Empresa>
+     */
+    Optional<Empresa> buscarPorCnpj(String cnpj);
+
+    /**
+     * Cadastra uma nova empresa na base de dados.
+     *
+     * @param empresa
+     * @return Empresa
+     */
+    Empresa persistir(Empresa empresa);
+
+}
